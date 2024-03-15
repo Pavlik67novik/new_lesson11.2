b01f359c43602698019b5ba896344dc5d852f339
Pavel Novikov <ya@pnovik.ru>
2024-03-15 18:31:33 +0300

    add func "title" and "docstring func"

--- str_func.py
+++ str_func.py
@@ -1,6 +1,12 @@
 x = input('Введите текст')
-
+""" первая функция значения Х слова пишет всеми заглавными буквами"""
 def capitalize(x):
   print(x.upper())
 
+"""функция заглавных первых букв в строке каждого слова """
+def text_title(x):
+  print(x.title())
+
+
 capitalize(x)
+text_title(x)
